60e594f67b4a75d83f0e6ac3c9fd08b04683220d
BereketK10 <79891681+BereketK10@users.noreply.github.com>
2021-11-17 01:14:39 -0800

    PA3 Multi-Threading

---
+++ kibret_CSCI201_Assignment2/src/kibret_CSCI201_Assignment2/jsonData.java
@@ -0,0 +1,18 @@
+ package kibret_CSCI201_Assignment2;
+
+public class jsonData {
+	private Stock[] data;
+
+	public Stock[] getData() {
+		return data;
+	}
+
+	public void setData(Stock[] data) {
+		this.data = data;
+	}
+	
+	public int getSize() {
+		return data.length;
+	}
+
+}
